5e431b9fcc77f17b8c2d3982803c9528f8e68660
Peter Inglesby <peter.inglesby@gmail.com>
2022-05-03 20:23:51 +0100

    Add Interface class

--- myrtle/interface.py
+++ myrtle/interface.py
@@ -1,47 +1,50 @@
-def call_function(ffi, lib, fn, ctx, argc, argv):
-    sqlite3_api = lib.sqlite3_api
-    SQLITE_TRANSIENT = ffi.cast("sqlite3_destructor_type", -1)
+class Interface:
+    def __init__(self, ffi, lib):
+        self.ffi = ffi
+        self.lib = lib
+        self.sqlite3_api = lib.sqlite3_api
+        self.SQLITE_TRANSIENT = ffi.cast("sqlite3_destructor_type", -1)
 
-    def _call_function(ctx, argc, argv):
-        py_args = [get_py_val(argv[ix]) for ix in range(argc)]
+    def call_function(self, ctx, fn, argc, argv):
+        py_args = [self.get_py_val(argv[ix]) for ix in range(argc)]
 
         try:
             result = fn(*py_args)
         except Exception:
-            set_result_error(ctx, fn)
+            self.set_result_error(ctx, fn)
 
-        set_result(ctx, result)
+        self.set_result(ctx, result)
 
-    def get_py_val(sqlite_value):
-        sqlite_type = sqlite3_api.value_type(sqlite_value)
+    def get_py_val(self, sqlite_value):
+        sqlite_type = self.sqlite3_api.value_type(sqlite_value)
         getters = {
-            lib.SQLITE_INTEGER: get_int_val,
-            lib.SQLITE_TEXT: get_str_val,
+            self.lib.SQLITE_INTEGER: self.get_int_val,
+            self.lib.SQLITE_TEXT: self.get_str_val,
         }
         return getters[sqlite_type](sqlite_value)
 
-    def get_int_val(sqlite_value):
-        return sqlite3_api.value_int64(sqlite_value)
+    def get_int_val(self, sqlite_value):
+        return self.sqlite3_api.value_int64(sqlite_value)
 
-    def get_str_val(sqlite_value):
-        return ffi.string(sqlite3_api.value_text(sqlite_value)).decode("utf8")
+    def get_str_val(self, sqlite_value):
+        return self.ffi.string(self.sqlite3_api.value_text(sqlite_value)).decode("utf8")
 
-    def set_result(ctx, result):
+    def set_result(self, ctx, result):
         setters = {
-            int: set_result_int,
-            str: set_result_text,
+            int: self.set_result_int,
+            str: self.set_result_text,
         }
         return setters[type(result)](ctx, result)
 
-    def set_result_int(ctx, result):
-        sqlite3_api.result_int64(ctx, result)
+    def set_result_int(self, ctx, result):
+        self.sqlite3_api.result_int64(ctx, result)
 
-    def set_result_text(ctx, result):
-        sqlite3_api.result_text(ctx, result.encode("utf8"), -1, SQLITE_TRANSIENT)
-
-    def set_result_error(ctx, fn):
-        sqlite3_api.result_error(
-            ctx, f"An exception occurred in {fn.__name__}".encode("utf8"), -1
+    def set_result_text(self, ctx, result):
+        self.sqlite3_api.result_text(
+            ctx, result.encode("utf8"), -1, self.SQLITE_TRANSIENT
         )
 
-    _call_function(ctx, argc, argv)
+    def set_result_error(self, ctx, fn):
+        self.sqlite3_api.result_error(
+            ctx, f"An exception occurred in {fn.__name__}".encode("utf8"), -1
+        )
--- myrtle/templates/plugin.py.tpl
+++ myrtle/templates/plugin.py.tpl
@@ -1,7 +1,9 @@
 from myrtle.import_helpers import import_attr
-from myrtle.interface import call_function
+from myrtle.interface import Interface
 
 from {{ extension_name }} import ffi, lib
+
+interface = Interface(ffi, lib)
 
 {% for fn in functions %}
 _{{ fn.namespaced_name }} = import_attr('{{ fn.path }}')
@@ -9,6 +11,6 @@ _{{ fn.namespaced_name }} = import_attr('{{ fn.path }}')
 
 @ffi.def_extern()
 def {{ fn.namespaced_name }}(ctx, argc, argv):
-    call_function(ffi, lib, _{{ fn.namespaced_name }}, ctx, argc, argv)
+    interface.call_function(ctx, _{{ fn.namespaced_name }}, argc, argv)
 
 {% endfor %}
